bd08b80e25e2cd642848888aa7a7bca8ca02c5fb
dsc <dsc@insilico.dk>
2015-02-24 14:09:10 +0100

    Change karma reporters - add clear-screen

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function (config) {
 
 		// test results reporter to use
 		// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-		reporters: ['progress'],
+		reporters: ['progress', 'clear-screen'],
 
 		// web server port
 		port: 9876,
